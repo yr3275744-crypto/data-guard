937286f27c2db779ef62e1a6196c0422e9a1245c
Mordy Pesach <mordypassover@gmail.com>
2026-07-20 17:50:14 +0300

    added interactiveClassificationManager file

---
+++ Managers/interactiveClassificationManager.cs
@@ -0,0 +1,17 @@
+using data_guard.ClassificateEmails;
+using data_guard.Interfaces;
+using data_guard.Storage;
+
+namespace data_guard.Managers;
+
+class InteractiveClassificationManager : ClassificationManager
+{
+	public InteractiveClassificationManager(ILogger logger, IEmailsReader emailsReader, EmailClassificator classificator, IModelExtractor modelExtractor, string rawDataName) : base(logger, emailsReader, classificator, modelExtractor, rawDataName)
+
+public void execute() { }
+
+	private string getEmailString()
+	{
+		
+	}
+}
