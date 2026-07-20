8b21c1ee35ec98f701a758207568b784b5519496
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-20 17:50:53 +0300

    add the interface and a class for it

---
+++ ClassificateEmails/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_guard.ClassificateEmails;
+
+class Model
+{
+    // ליצור את השדות האלו:
+    //(string[], Dictionary<string, int>, Dictionary<(string, string), int>, Dictionary<(string, string, int), int>)
+}
