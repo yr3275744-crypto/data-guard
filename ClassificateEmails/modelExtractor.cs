cf561b3c47a815f0b503df0e4a83d60adf2e6009
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-21 10:34:38 +0300

    write first methode in extractor

--- ClassificateEmails/modelExtractor.cs
+++ ClassificateEmails/modelExtractor.cs
@@ -2,9 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace data_guard.ClassificateEmails
+namespace data_guard.ClassificateEmails;
+
+class ModelExtractor
 {
-    internal class modelExtractor
+    public List<Dictionary<string, string>> RawTable { get; private set; }
+
+    public string[] Keys { get; private set; }
+    public string[]? Labels { get; private set; }
+    public Dictionary<string, int>? Priors { get; private set; }
+    public Dictionary<(string, string, int), int>? Cond { get; private set; }
+    public Dictionary<(string, string), int>? Unseen { get; private set; }
+
+    public ModelExtractor(List<Dictionary<string, string>> rawTable, string[] keys)
     {
+        RawTable = rawTable;
+        Keys = keys;
+        Labels = GetLabels();
+    }
+    private string[] GetLabels()
+    {
+        //int rowsNumber = RawTable.Count;
+        string[] labels = RawTable.GroupBy(row => row[Keys[Keys.Length - 1]])
+            .Select(grop => grop.Key)
+            .ToArray();
+        return labels;
     }
 }
