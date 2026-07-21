51d7af76215f518548e92270172077b6da221f9d
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-21 15:25:40 +0300

    first version of ModelExtractor

--- ClassificateEmails/modelExtractor.cs
+++ ClassificateEmails/modelExtractor.cs
@@ -10,8 +10,9 @@ namespace data_guard.ClassificateEmails;
 class ModelExtractor
 {
     public List<Dictionary<string, string>> RawTable { get; private set; }
+    public string[]? Features { get; private set; }
 
-    public string[]? Keys { get; private set; }
+    public string TargetColumnKey { get; private set; }
     public List<string> Labels { get; private set; }
     public Dictionary<string, double> Priors { get; private set; }
     public Dictionary<(string, string, string), double> Cond { get; private set; }
@@ -20,42 +21,76 @@ class ModelExtractor
     public ModelExtractor()
     {
         RawTable = new();
+        TargetColumnKey = "";
         Labels = new();
         Priors = new();
         Cond = new();
         Unseen = new();
     }
-    public Model GetModel(List<Dictionary<string, string>> rawTable, string[] keys)
+    public Model GetModel(List<Dictionary<string, string>> rawTable)
     {
-        if (keys.Length == 0)
-        {
-            throw new UnClassificationTable("Not enough columns");
-        }
         if (rawTable.Count == 0)
         {
             throw new UnClassificationTable("Not enough rows");
         }
+
         RawTable = rawTable;
-        Keys = keys;
-        //Labels = GetLabels();
-        //Priors = GetPriors();
+        DefineFeatuersAndTarget();
+        DefineLabels();
+        DefinePriors();
+        DefineCondAndUnseen();
+        return new Model(Labels, Priors, Cond, Unseen);
+
+    }
+    private void DefineFeatuersAndTarget()
+    {
+        Features = RawTable[0].Keys.ToArray();
+        if (Features.Length < 2)
+        {
+            throw new UnClassificationTable("Not enough columns");
+        }
+        TargetColumnKey = Features[Features.Length - 1];
     }
     private void DefineLabels()
     {
         //int rowsNumber = RawTable.Count;
-        Labels = RawTable.GroupBy(row => row[Keys[Keys.Length - 1]])
+        Labels = RawTable.GroupBy(row => row[TargetColumnKey])
             .Select(grop => grop.Key)
             .ToList();
     }
-    private void definePriors()
+    private void DefinePriors()
     {
         foreach (string label in Labels)
         {
-            Priors[label] = RawTable.Count(row => row[Keys[Keys.Length - 1]] == label) / (double)RawTable.Count();
+            Priors[label] = RawTable.Count(row => row[TargetColumnKey] == label) / (double)RawTable.Count();
         }
     }
-    private void DefineCond()
+    private void DefineCondAndUnseen()
     {
-
+        foreach (string label in Labels)
+        {
+            foreach (string feature in Features[..(Features.Length - 1)])
+            {
+                List<string> values = RawTable
+                    .Where(row => row[TargetColumnKey] == label)
+                    .GroupBy(row => row[feature])
+                    .Select(g => g.Key)
+                    .ToList();
+                foreach (string value in values)
+                {
+                    int count = RawTable
+                        .Where(row => row[TargetColumnKey] == label)
+                        .Count(row => row[feature] == value);
+                    if (count != 0)
+                    {
+                        Cond[(label, feature, value)] = count / (double)RawTable.Count(row => row[TargetColumnKey] == label);
+                    }
+                    else
+                    {
+                        Unseen[(label, feature)] = 1 / ((double)RawTable.Count(row => row[TargetColumnKey] == label) + values.Count);
+                    }
+                }
+            }
+        }
     }
 }
