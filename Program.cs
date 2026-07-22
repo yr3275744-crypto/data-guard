557f1c15470906e8c1781112474d903ea1bfbdac
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-22 10:16:07 +0300

    fix line in program

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ namespace data_guard
         {
             // Initialize dependencies
             ILogger logger = new ConsoleLogger();
-            PathCreator pathCreator = new PathCreator(); 
-            IModelExtractor modelExtractor = new ModelExtractor(); 
+            PathCreator pathCreator = new PathCreator();
+            IModelExtractor modelExtractor = new ModelExtractor();
             IEmailsTraningReader emailTraningReader = new EmailsTraningReader();
             EmailClassificator classificator = new EmailClassificator();
 
@@ -34,7 +34,7 @@ namespace data_guard
                     break;
 
                 default:
-                    Console.WriteLine("Cannot run program, must receive 1 or 2 arguments!");
+                    Console.WriteLine("Cannot run the program, it must receive 1 or 2 arguments");
                     break;
 
             }
