643c8f10266b833140ac9191fcae0b93bd55cae6
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-22 09:31:09 +0300

    write the writer

---
+++ Storage/EmailsWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using data_guard.Interfaces;
+
+namespace data_guard.Storage;
+
+class EmailsWriter : IEmailsWriter
+{
+    public void Write(string target, List<string> emails)
+    {
+        string path = PathCreator.GetPath(target);
+        File.WriteAllLines(path, emails);
+    }
+}
+
